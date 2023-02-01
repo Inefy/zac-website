c6b42ec84b0879c75bad89eaa7e34751524dd36e
Inefy <zbatten@mun.ca>
2023-02-01 16:16:24 -0330

    initial website

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask 
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello world</hi>"
+def main():
+    return render_template("main.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run()
